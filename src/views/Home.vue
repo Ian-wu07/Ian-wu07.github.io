97306a2818d28602e1b0d93e3f90658647e04659
Ian-wu07 <ianandvivi310@gmail.com>
2022-06-16 21:52:33 +0800

    1.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import MdiGithub from "~icons/mdi/github";
 import MdiInstagram from "~icons/mdi/instagram";
 import Typing from "../components/Typing.vue";
+import Project from "../components/Project.vue";
 
 let T = ref(0);
 let timer_id = setInterval(() => {
@@ -15,7 +16,7 @@ let timer_id = setInterval(() => {
 <template>
     <div class="w-full overflow-y-auto px-8 pt-16 sm:px-12 sm:pt-20 lg:px-16 lg:pt-24">
         <Typing text="My brief" class="mb-2 block text-2xl sm:text-3xl lg:text-4xl" />
-        <Typing v-if="T > 1200" text="Hi! Ian" :speed="60" />
+        <Typing v-if="T > 1200" text="Hi!" :speed="60" />
         <a
             v-if="T > 1600"
             class="mt-8 flex items-center text-lg text-gray-500 transition-all hover:text-gray-700"
@@ -34,5 +35,6 @@ let timer_id = setInterval(() => {
             <MdiInstagram class="mr-1 inline-block" />
             <Typing v-if="T > 2000" text="Instagram" :speed="60" />
         </a>
+        <Project name="Ian" description="program so hard" image="https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"/>
     </div>
 </template>
